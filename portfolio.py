1f7e289e8da0b8d2b61f4533486a32c0e1634ecb
Austin Hochman <a.w.hochman@gmail.com>
2021-05-02 12:09:18 -0400

    Rebalancing

--- portfolio.py
+++ portfolio.py
@@ -1,3 +1,33 @@
+import datetime
+
+COMM = 'Communication'
+UTIL = 'Utilities'
+ENER = 'Energy'
+RE = 'Real_Estate'
+CONS = 'Cons_Staples'
+HC = 'Healthcare'
+FIN = 'Financials'
+IND = 'Industrials'
+MAT = 'Materials'
+COND = 'Cons_Disc'
+IT = 'Info_Tech'
+
+IDEAL_RATIOS = {
+    COMM: 0.144623,
+    UTIL: 0.146204,
+    ENER: 0.067658,
+    RE: 0.075623,
+    CONS: 0.153964,
+    HC: 0.127841,
+    FIN: 0.036614,
+    IND: 0.049434,
+    MAT: 0.088559,
+    COND: 0.062321,
+    IT: 0.047160
+}
+
+SECTORS = [COMM, UTIL, ENER, RE, CONS, HC, FIN, IND, MAT, COND, IT]
+
 class Portfolio():
 
     """
@@ -13,51 +43,74 @@ class Portfolio():
     Returns: 
         Portfolio Object 
     """
-    def __init__(self, w_to_change, timeline, positions):
-        self.willing_to_change = w_to_change
-        self.timeline = timeline
-        self.positions = positions
-        self.num_stocks = self.get_num_stocks()
+    def __init__(self):
         self.amt_categories = {
-            'c1': 0,
-            'c2': 0,
-            'c3': 0,
-            'c4': 0,
-            'c5': 0
-        }
-        self.new_category_ratios = {
-            'c1': .2,
-            'c2': .2,
-            'c3': .2,
-            'c4': .2,
-            'c5': .2
+            COMM: 100,
+            UTIL: 100,
+            RE: 100,
+            CONS: 100,
+            HC: 100,
+            FIN: 100,
+            IND: 100,
+            MAT: 100,
+            COND: 100,
+            IT: 100
         }
 
-    """
-    Counts the number of individual stocks that a person has in their portfolio. 
-
-    Returns:
-        int
-    """
-    def get_num_stocks(self):
-        n = 0 
-        for p in self.positions:
-            n += self.positions[p] 
-        return n 
 
     """
-    Uses static data to categorize the positions inside a portfolio. It updates
-    the amt_categories dictionary within the Portfolio object. 
-    """
-    def categorize_positions(self):
-        pass 
+    Returns the current values of each sector.
 
-    """
-    Uses the amt_categories attribute and num_positions attribute to determine 
-    what the initial ratios for each category in the inital portfolio were. 
+    Parameter:
+        date: datetime.datetime
 
     Returns:
         {string: float} dict
     """
-    def initial_cat_ratios(self):
-        pass
+    def get_sector_values(self, date):
+        return {
+            COMM: 0,
+            UTIL: 0,
+            RE: 0,
+            CONS: 0,
+            HC: 0,
+            FIN: 0,
+            IND: 0,
+            MAT: 0,
+            COND: 0,
+            IT: 0
+        }
+
+
+    """
+    Returns the current value of the portfolio.
+
+    Parameter:
+        date: datetime.datetime
+
+    Returns:
+        int
+    """
+    def get_portfolio_value(self, date):
+        value = 0 
+        for p in self.amt_categories:
+            value += self.amt_categories[p]
+        return value
+
+
+    """
+    Updates the amt_categories attribute of the current portfolio to reach the 
+    'ideal' distribution again.
+
+    Parameter:
+        date: datetime.datetime
+    """
+    def rebalance_portfolio(self, date):
+        portfolio_value = self.get_portfolio_value(date)
+        amts_to_invest = {}
+        for s in SECTORS:
+            amt_sector = portfolio_value * IDEAL_RATIOS[s]
+            amts_to_invest[s] = amt_sector
+        self.amt_categories = amts_to_invest
+
+
